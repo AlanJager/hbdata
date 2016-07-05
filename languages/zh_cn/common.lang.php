72a050b80ae4a87da3f367eae58b31c0fb0d47c5
ZyanZou <alanjager@outlook.com>
2016-07-05 09:44:43 +0800

    finish

---
+++ languages/zh_cn/common.lang.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * hbData
+ * --------------------------------------------------------------------------------------------------
+ * 版权所有 2016-
+ * 网站地址:
+ * --------------------------------------------------------------------------------------------------
+ * Author: AlanJager
+ * Release Date: 2016-7-4
+ */
+
+// 公共语言项
+$_LANG['ur_here'] = '当前位置';
+$_LANG['home'] = '首页';
+$_LANG['add_time'] = '发布时间';
+$_LANG['click'] = '点击数';
+$_LANG['price'] = '价格';
+$_LANG['price_format'] = '￥d% 元';
+$_LANG['price_discuss'] = '面议';
+$_LANG['copyright'] = '© d% 2016 All Rights Reserved.';
+$_LANG['powered_by'] = "<a href='http://www.douco.com' target='_blank'>Powered by DouPHP</a>";
+$_LANG['home'] = '首页';
+$_LANG['add_favorite'] = '收藏本站';
+$_LANG['link'] = '友情链接';
+$_LANG['site_closed'] = '站点维护升级中，请您稍后再来...';
+$_LANG['btn_submit'] = '提交';
+$_LANG['select'] = '请选择...';
+$_LANG['captcha'] = '验证码';
+$_LANG['captcha_wrong'] = '验证码错误或者为空！';
+$_LANG['captcha_refresh'] = '看不清？点击更换另一个验证码。';
+$_LANG['page_wrong'] = '页面不存在！';
+$_LANG['illegal'] = '操作有误';
+$_LANG['illegal_char'] = '中含有非法字符';
+$_LANG['go_top'] = '返回顶部';
+$_LANG['dou_pc'] = '电脑版';
+$_LANG['dou_mobile'] = '手机版';
+$_LANG['number'] = '数量';
+$_LANG['handler'] = '操作';
+$_LANG['view'] = '查看';
+$_LANG['del'] = '删除';
+$_LANG['yes'] = '是';
+$_LANG['no'] = '否';
+$_LANG['mail_altbody'] = '要查看信息，请使用兼容HTML的电子邮件查看器！';
+$_LANG['mail_send_success'] = '邮件发送成功';
+$_LANG['mail_send_fail'] = '邮件发送失败，请重试';
+
+// 公司简介
+$_LANG['about'] = '公司简介';
+$_LANG['about_link'] = '查看详细';
+$_LANG['about_tree'] = '公司信息';
+
+// 联系我们
+$_LANG['contact'] = '联系我们';
+$_LANG['contact_address'] = '公司地址';
+$_LANG['contact_tel'] = '销售热线';
+$_LANG['contact_fax'] = '传真号码';
+$_LANG['contact_root_url'] = '公司网址';
+$_LANG['contact_email'] = '电子邮箱';
+
+// 搜索
+$_LANG['search'] = '搜索';
+$_LANG['search_results'] = "搜索结果：‘d%’";
+$_LANG['search_product'] = '产品搜索';
+$_LANG['search_product_cue'] = '输入您要搜索的产品名称';
+$_LANG['search_article'] = '文章搜索';
+$_LANG['search_article_cue'] = '输入您要搜索的文章名称';
+$_LANG['search_keyword_wrong'] = '搜索词只能包含中文、英文、数字及下划线';
+
+// 分页
+$_LANG['pager_1'] = '总计';
+$_LANG['pager_2'] = '个记录';
+$_LANG['pager_3'] = '共';
+$_LANG['pager_4'] = '页';
+$_LANG['pager_5'] = '当前第';
+$_LANG['pager_6'] = '第';
+$_LANG['pager_first'] = '第一页';
+$_LANG['pager_last'] = '最末页';
+$_LANG['pager_previous'] = '上一页';
+$_LANG['pager_next'] = '下一页';
+
+// 在线客服
+$_LANG['online'] = '在线客服';
+$_LANG['online_qq'] = 'QQ交谈';
+
+// 提示信息
+$_LANG['dou_msg'] = '信息提示';
+$_LANG['dou_msg_cue'] = "如果您不做出选择，将在 d% 秒后跳转到上一页面，您也可以手动点击";
+$_LANG['dou_msg_back'] = '返回上一页';
+$_LANG['dou_msg_success'] = '操作已成功';
---
+++ languages/zh_cn/guestbook.lang.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * hbData
+ * --------------------------------------------------------------------------------------------------
+ * 版权所有 2016-
+ * 网站地址:
+ * --------------------------------------------------------------------------------------------------
+ * Author: AlanJager
+ * Release Date: 2016-7-4
+ */
+
+// 留言板
+$_LANG['guestbook'] = '留言反馈';
+$_LANG['guestbook_add'] = '我要留言';
+$_LANG['guestbook_title'] = '留言主题';
+$_LANG['guestbook_name'] = '联系人';
+$_LANG['guestbook_contact_type'] = '回复方式';
+$_LANG['guestbook_tel'] = '电话或手机';
+$_LANG['guestbook_email'] = 'E-MAIL';
+$_LANG['guestbook_qq'] = '腾讯QQ';
+$_LANG['guestbook_content'] = '留言内容';
+$_LANG['guestbook_insert_success'] = '留言成功，请等待管理员与您取得联系';
+$_LANG['guestbook_contact_type_empty'] = '请选择一种回复方式';
+$_LANG['guestbook_title_wrong'] = '主题为空或长度超过限制，d%请重新输入';
+$_LANG['guestbook_name_wrong'] = '联系人为空或长度超过限制，d%请重新输入';
+$_LANG['guestbook_email_wrong'] = 'E-MAIL地址为空或输入错误，请重新输入';
+$_LANG['guestbook_tel_wrong'] = '电话号码不能为空且只能输入数字，请重新输入';
+$_LANG['guestbook_qq_wrong'] = 'QQ号码不能为空且只能输入数字，请重新输入';
+$_LANG['guestbook_content_wrong'] = '留言内容为空或长度超过限制，d%请重新输入';
+$_LANG['guestbook_include_chinese'] = '且必须包含中文，';
+$_LANG['guestbook_is_water'] = '您已经发表了 3 次留言，请您耐心等待管理员回复';
+$_LANG['guestbook_reply'] = '网站回复';
