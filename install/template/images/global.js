b6d819c2c0aa473b97ff22087887ccfae59809f7
ZyanZou <alanjager@outlook.com>
2016-07-05 10:26:59 +0800

    add js funs

---
+++ install/template/images/global.js
@@ -0,0 +1,23 @@
+/**
+/**
+ +----------------------------------------------------------
+ * 表单提交
+ +----------------------------------------------------------
+ */
+function habataSubmit(form_id) {
+	   var formParam = $("#"+form_id).serialize(); //序列化表格内容为字符串
+    
+    $.ajax({
+        type: "POST",
+        url: $("#"+form_id).attr("action")+'&do=callback',
+        data: formParam,
+        dataType: "html",
+        success: function(html) {
+									   if (!html) {
+																$("#"+form_id).submit();
+												} else {
+																$("#cue").html(html);
+												}
+        }
+    });
+}
