a772b47c7af07478c4b1c9496417d9829fb979a4
邓 <1351650853@qq.com>
2016-07-13 22:50:14 +0800

    后台首页加入权限认证

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,11 @@ define('IN_HBDATA', true);
 
 require (dirname(__FILE__) . '/include/init.php');
 
+//权限判断
+require ('auth.php');
+
+
+
 // rec操作项的初始化
 $rec = $check->is_rec($_REQUEST['rec']) ? $_REQUEST['rec'] : 'default';
 
