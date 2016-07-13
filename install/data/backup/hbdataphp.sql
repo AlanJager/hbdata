f26683594b5dd18fde7e7ac082164b6fb1d9604a
邓 <1351650853@qq.com>
2016-07-14 02:51:18 +0800

    导入角色权限表

--- install/data/backup/hbdataphp.sql
+++ install/data/backup/hbdataphp.sql
@@ -14,6 +14,8 @@ CREATE TABLE `hbdata_permissions` (
   KEY `Rght` (`Rght`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
 
+
+
 DROP TABLE IF EXISTS `hbdata_rolepermissions`;
 CREATE TABLE `hbdata_rolepermissions` (
   `RoleID` int(11) NOT NULL,
@@ -21,6 +23,90 @@ CREATE TABLE `hbdata_rolepermissions` (
   `AssignmentDate` int(11) NOT NULL,
   PRIMARY KEY  (`RoleID`,`PermissionID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 1, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 2, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 3, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 4, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 5, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 6, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 7, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 8, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 9, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 10, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 11, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 12, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 13, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 14, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 15, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 16, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 17, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 18, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 19, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 20, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 21, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 22, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 23, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 24, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 25, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 26, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 27, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 28, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 29, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 30, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 31, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 32, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 33, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 34, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 35, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 36, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 37, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 38, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 39, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 40, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 41, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 42, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 43, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 44, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 45, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 46, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 47, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 48, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 49, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 50, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 51, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 52, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 53, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 54, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 55, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 56, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 57, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 58, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 59, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 60, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 61, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 62, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 63, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 64, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 65, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 66, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 67, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 68, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 69, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 70, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 71, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 72, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 73, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 74, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 75, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 76, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 77, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 78, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 79, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 80, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 81, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 82, 0);
+INSERT INTO `hbdata_rolepermissions` VALUES (1, 83, 0);
 
 DROP TABLE IF EXISTS `hbdata_roles`;
 CREATE TABLE`hbdata_roles` (
