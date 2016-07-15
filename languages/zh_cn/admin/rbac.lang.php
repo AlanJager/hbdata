1d53bdc0e1ce5dbfa86e4d7f47782d9d5dc968de
ZyanZou <alanjager@outlook.com>
2016-07-15 15:12:22 +0800

    fixed lang pros

--- languages/zh_cn/admin/rbac.lang.php
+++ languages/zh_cn/admin/rbac.lang.php
@@ -22,7 +22,7 @@ $_LANG['add_permission'] = '添加权限';
 $_LANG['permission_add_success'] = '添加权限成功';
 
 //用户角色管理
-$_LANG['user_role_add_time'] = '权限描述';
-$_LANG['user_role_list'] = '返回角色列表';
-$_LANG['add_user_role'] = '添加权限';
-$_LANG['user_role_add_success'] = '添加权限成功';
+$_LANG['user_role_assign_date'] = '用户角色添加日期';
+$_LANG['user_role_list'] = '返回用户角色列表';
+$_LANG['add_user_role'] = '添加用户角色';
+$_LANG['user_role_add_success'] = '添加用户角色成功';
