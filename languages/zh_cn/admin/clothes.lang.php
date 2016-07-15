46d0b5fb9a466f77d1aecd4114b098d62922cee6
邓 <1351650853@qq.com>
2016-07-15 17:40:10 +0800

    item页面修正

---
+++ languages/zh_cn/admin/clothes.lang.php
@@ -0,0 +1,38 @@
+<?php
+//category
+$_LANG['clothes'] = '衣服';
+$_LANG['clothes_category'] = '衣服分类';
+
+//系统设置
+$_LANG['top_add_clothes'] = '衣服';
+$_LANG['display_clothes'] = '衣服列表数量';
+$_LANG['display_home_clothes'] = '首页展示衣服数量';
+$_LANG['defined_clothes'] = '衣服自定义属性';
+$_LANG['defined_clothes_cue'] = '如"颜色,尺寸,型号"中间以英文逗号隔开';
+$_LANG['sort_clothes'] = '开始筛选首页衣服';
+$_LANG['nav_clothes'] = '衣服中心';
+$_LANG['mobile_display_clothes'] = '手机版衣服列表数量';
+$_LANG['mobile_display_home_clothes'] = '手机版首页展示衣服数量';
+
+//衣服分类
+$_LANG['clothes_category'] = '衣服分类';
+$_LANG['clothes_category_add'] = '添加分类';
+$_LANG['clothes_category_edit'] = '编辑衣服分类';
+$_LANG['clothes_category_del'] = '删除衣服分类';
+$_LANG['clothes_category_del_is_parent'] = '"d%"不是末级分类或者分类下还存在衣服，您不能删除';
+$_LANG['clothes_category_name'] = '分类名称';
+$_LANG['clothes_category_add_succes'] = '添加衣服分类成功';
+$_LANG['clothes_category_edit_succes'] = '编辑衣服分类成功';
+
+//衣服中心
+$_LANG['clothes'] = '衣服';
+$_LANG['clothes_add'] = '添加衣服';
+$_LANG['clothes_edit'] = '编辑衣服';
+$_LANG['clothes_del'] = '删除衣服';
+$_LANG['clothes_name'] = '衣服名称';
+$_LANG['clothes_defined'] = '自定义属性';
+$_LANG['clothes_defined_cue'] = '以英文逗号 , 隔开';
+$_LANG['clothes_content'] = '衣服描述';
+$_LANG['clothes_add_succes'] = '添加衣服成功';
+$_LANG['clothes_edit_succes'] = '编辑衣服成功';
+$_LANG['clothes_select_empty'] = '没有选择任何衣服';
